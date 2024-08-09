c027d496140de987412dceea33f06ef74ede738e
Prince Bright <dankwaprince@gmail.com>
2024-08-09 23:29:53 +0000

    sms

--- app/Http/Controllers/VourcherController.php
+++ app/Http/Controllers/VourcherController.php
@@ -49,15 +49,46 @@ class VourcherController extends Controller
             $data = Vourchers::query()->where('amount', $amount)->where('status', 'UNUSED')->first();
             if ($data) {
                 //Send SMS
-
+                $msg = 'Payment Successfully and your Voucher Code is: ' .$data->vourcher;
+                $this->sendSMSMobile($phone,$msg);
                 //Update Vourcher Table
                 $data->status ='USED';
                 $data->user_assigned = $phone;
                 $data->save();
+                return ResponseController::responseSuccess("ok",$data);
             }
         } catch (\Exception $exception) {
             return ResponseController::responseError($exception->getMessage());
         }
 
+    }
+
+    public function sendSMSMobile($phone,$msg){
+        $endPoint = 'https://api.mnotify.com/api/sms/quick';
+        $apiKey = env('SMS_KEY');
+        $url = $endPoint . '?key=' . $apiKey;
+        $data = [
+            'recipient'=>[$phone],
+            'sender'=>'Signal_WiFi',
+            'message'=>$msg,
+            'is_schedule'=>'false',
+            'schedule_date'=>''
+        ];
+        $ch = curl_init();
+        $headers = array();
+        $headers[] = "Content-Type: application/json";
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        $result = curl_exec($ch);
+        $result = json_decode($result, TRUE);
+        curl_close($ch);
+        if($result['code'] === 2000 ){
+            return $result['message'];
+        }else{
+            return 0;
+        }
     }
 }
